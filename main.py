d52ea6d2df1c25058e2f290e28ddefba4f424987
MikaJanBales <114147741+MikaJanBales@users.noreply.github.com>
2022-12-01 21:00:50 +0300

    Fifth commit

--- main.py
+++ main.py
@@ -2,49 +2,68 @@ class TreeStore:
     def __init__(self, items_task):
         self.items_task = items_task
         self.items_full = self.__create_items_full()
-        self.parents_answer = []
 
     def __create_items_full(self):
         items_full = dict()
-        for item in self.items_task:
-            items_full[item["id"]] = {}
-            items_full[item["id"]]["parent"] = item["parent"]
-            if "type" in item:
-                items_full[item["id"]]["type"] = item["type"]
-            items_full[item["id"]]["children"] = []
-            if item["parent"] != "root":
-                items_full[item["parent"]]["children"].append(item["id"])
-            # self.items_full[item['id']]['item_list_index'] = item['id'] - 1
-        return items_full
+        for index, item in enumerate(self.items_task):
+            if not items_full.get(item['id']):
+                items_full[item['id']] = {
+                    'parent': item['parent'],
+                    'children': [],
+                    'item_list_index': index,
+                }
+            else:
+                items_full[item['id']].update(
+                    {
+                        'parent': item['parent'],
+                        'item_list_index': index,
+                    }
+                )
 
-    def __formater(self, list_before):
-        answer = []
-        for elem_list_before in list_before:
-            answer.append(self.items_task[elem_list_before - 1])
-        return answer
+            if item['parent'] != 'root':
+                if items_full.get(item['parent']):
+                    (
+                        items_full[item['parent']]['children']
+                        .append(item['id'])
+                    )
+                else:
+                    items_full[item['parent']] = {
+                        'children': [item['id']]
+                    }
+        return items_full
 
     def getAll(self):
         return self.items_task
 
     def getItem(self, id_elem):
-        item_answer = {}
-        item_answer["id"] = id_elem
-        item_answer["parent"] = self.items_full[id_elem]["parent"]
-        if "type" in self.items_full[id_elem]:
-            item_answer["type"] = self.items_full[id_elem]["type"]
-        return item_answer
+        try:
+            item_list_index = self.items_full[id_elem]['item_list_index']
+            return self.items_task[item_list_index]
+        except KeyError:
+            return 'Not Found'
 
     def getChildren(self, id_elem):
-        children_answer = self.items_full[id_elem]["children"]
-        return self.__formater(children_answer)
+        try:
+            result = []
+            children = self.items_full[id_elem]['children']
+            for child in children:
+                child_index = self.items_full[child]['item_list_index']
+                result.append(self.items_task[child_index])
+            return result
+        except KeyError:
+            return 'Not Found'
 
     def getAllParents(self, id_elem):
-        parent = self.items_full[id_elem]['parent']
-        self.parents_answer.append(self.getItem(parent))
-        if parent != 'root':
-            self.getAllParents(parent)
-        else:
-            return self.parents_answer
+        try:
+            parents_answer = []
+            while self.items_full[id_elem]['parent'] != 'root':
+                parent = self.items_full[id_elem]['parent']
+                parent_index = self.items_full[parent]['item_list_index']
+                parents_answer.append(self.items_task[parent_index])
+                id_elem = parent
+            return parents_answer
+        except KeyError:
+            return 'Not Found'
 
 
 items = [
@@ -60,6 +79,6 @@ items = [
 ts = TreeStore(items)
 
 # print(ts.getAll())
-print(ts.getItem(7))
-# print(ts.getChildren(4))
-# print(ts.getAllParents(2))
+# print(ts.getItem(7))
+# print(ts.getChildren(2))
+# print(ts.getAllParents(7))
